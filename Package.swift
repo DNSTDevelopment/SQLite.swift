aed6123a3ef380569fd5c53a610611364489a1a0
DNSTDevelopment <138738804+DNSTDevelopment@users.noreply.github.com>
2024-01-16 10:45:02 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ let package = Package(
     targets: [
         .target(
             name: "SQLite",
-            swiftSettings: [.unsafeFlags(["-enable-library-evolution","-emit-module-interface"])],
             exclude: [
                 "Info.plist"
             ]
